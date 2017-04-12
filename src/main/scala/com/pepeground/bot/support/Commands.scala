2d41ca4470ba5488efb3b0cb3bed21c24077466a
Kirill Zaitsev <kirik910@gmail.com>
2017-04-13 02:04:11 +0300

    Remove usless commands

--- src/main/scala/com/pepeground/bot/support/Commands.scala
+++
@@ -1,6 +0,0 @@
-package com.pepeground.bot.support
-
-trait ResponseCommand
-class Reply(val str: Option[String]) extends ResponseCommand
-class Answer(val str: Option[String]) extends ResponseCommand
-class Silence extends ResponseCommand
